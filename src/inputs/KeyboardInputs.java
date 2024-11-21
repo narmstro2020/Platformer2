0907882cd45c0ec9fab28a2e26605b8ab7ec2eac
Nicholas Armstrong <narmstro@warren.k12.in.us>
2024-11-21 18:14:38 -0500

    Part 04

--- src/inputs/KeyboardInputs.java
+++ src/inputs/KeyboardInputs.java
@@ -1,35 +1,49 @@
 package inputs;
 
-import main.GamePanel;
-
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
+import main.Game;
+import main.GamePanel;
+
 public class KeyboardInputs implements KeyListener {
 
-    private GamePanel gamePanel;
+	private GamePanel gamePanel;
 
-    public KeyboardInputs(GamePanel gamePanel) {
-        this.gamePanel = gamePanel;
-    }
+	public KeyboardInputs(GamePanel gamePanel) {
+		this.gamePanel = gamePanel;
+	}
 
-    @Override
-    public void keyTyped(KeyEvent e) {
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
 
-    }
+	}
 
-    @Override
-    public void keyPressed(KeyEvent e) {
-        switch (e.getKeyChar()) {
-            case KeyEvent.VK_W -> gamePanel.changeYDelta(-5);
-            case KeyEvent.VK_A -> gamePanel.changeXDelta(-5);
-            case KeyEvent.VK_S -> gamePanel.changeXDelta(5);
-            case KeyEvent.VK_D -> gamePanel.changeYDelta(5);
-        }
-    }
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
 
-    @Override
-    public void keyReleased(KeyEvent e) {
+	}
 
-    }
+	@Override
+	public void keyPressed(KeyEvent e) {
+
+		switch (e.getKeyCode()) {
+		case KeyEvent.VK_W:
+			gamePanel.changeYDelta(-5);
+			break;
+		case KeyEvent.VK_A:
+			gamePanel.changeXDelta(-5);
+			break;
+		case KeyEvent.VK_S:
+			gamePanel.changeYDelta(5);
+			break;
+		case KeyEvent.VK_D:
+			gamePanel.changeXDelta(5);
+			break;
+		}
+
+	}
+
 }
--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -1,44 +1,76 @@
 package main;
 
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.imageio.ImageIO;
+import javax.swing.JPanel;
+
 import inputs.KeyboardInputs;
 import inputs.MouseInputs;
 
-import javax.swing.*;
-import java.awt.*;
-
 public class GamePanel extends JPanel {
 
-    private int xDelta = 100;
-    private int yDelta = 100;
-    private MouseInputs mouseInputs;
+	private MouseInputs mouseInputs;
+	private float xDelta = 100, yDelta = 100;
+	private BufferedImage img, subImg;
 
-    public GamePanel() {
-        mouseInputs = new MouseInputs(this);
-        addMouseListener(mouseInputs);
-        addMouseMotionListener(mouseInputs);
-        addKeyListener(new KeyboardInputs(this));
-    }
+	public GamePanel() {
 
-    public void changeXDelta(int value){
-        xDelta += value;
-        repaint();
-    }
+		mouseInputs = new MouseInputs(this);
 
-    public void changeYDelta(int value){
-        yDelta += value;
-        repaint();
-    }
+		importImg();
 
-    public void setRectPos(int x, int y){
-        xDelta += x;
-        yDelta += y;
-        repaint();
-    }
+		setPanelSize();
+		addKeyListener(new KeyboardInputs(this));
+		addMouseListener(mouseInputs);
+		addMouseMotionListener(mouseInputs);
 
-    @Override
-    public void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        g.fillRect(xDelta,  yDelta, 200, 50);
+	}
 
-    }
+	private void importImg() {
+		InputStream is = getClass().getResourceAsStream("/player_sprites.png");
+
+		try {
+			img = ImageIO.read(is);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	private void setPanelSize() {
+		Dimension size = new Dimension(1280, 800);
+		setMinimumSize(size);
+		setPreferredSize(size);
+		setMaximumSize(size);
+
+	}
+
+	public void changeXDelta(int value) {
+		this.xDelta += value;
+
+	}
+
+	public void changeYDelta(int value) {
+		this.yDelta += value;
+
+	}
+
+	public void setRectPos(int x, int y) {
+		this.xDelta = x;
+		this.yDelta = y;
+	}
+
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+
+		subImg = img.getSubimage(1 * 64, 8 * 40, 64, 40);
+		g.drawImage(subImg, (int) xDelta, (int) yDelta, 128, 80, null);
+
+	}
+
 }
